5695490ce60e48a319b25426827f6d137064d672
HwyToHell <holger.grzonka@web.de>
2020-08-24 15:29:47 +0200

    188 form validation

--- src/app/15-forms/app.component.ts
+++ src/app/15-forms/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,12 +7,25 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // use ViewChild method, pointing to local reference in template
+  @ViewChild('form') signupForm: NgForm;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
 
+  // use @ViewChild method
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+
+  // use NgForm model method
+  /* 
   onSubmit(form: NgForm) {
     console.log("submitted");
-    console.log(form.value);
+    console.log(form);
+    //console.log(form.value);
   }
+  */
 }
