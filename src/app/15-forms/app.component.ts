28aa6cde4223f9d07cf503988d0c7de63c2aa62a
HwyToHell <holger.grzonka@web.de>
2020-08-26 15:34:15 +0200

    197 setting and patching form values

--- src/app/15-forms/app.component.ts
+++ src/app/15-forms/app.component.ts
@@ -9,15 +9,38 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   // use ViewChild method, pointing to local reference in template
   @ViewChild('form') signupForm: NgForm;
+  defaultQuestion = "pet";
+  answer = '';
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    /* //@ViewChild approach
+    this.signupForm.setValue({
+      userData: {
+        username: suggestedName,
+        emailaddress: '' 
+      },
+      secret: 'teacher',
+      questionAnswer: '',
+      gender: 'male'
+    }); */
+
+    // use form.patchValue() method
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      },
+      secret: 'teacher',
+      gender: 'male'
+    });
+
   }
 
   // use @ViewChild method
   onSubmit() {
-    //console.log(this.signupForm);
-    console.log(this.signupForm.valid);
+    console.log(this.signupForm);
+    //console.log(this.signupForm.valid);
   }
 
 
