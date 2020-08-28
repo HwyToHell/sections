cb9a75823aa8448a2c6852664111ebcf3f63c469
HwyToHell <holger.grzonka@web.de>
2020-08-28 23:43:40 +0200

    assignment 6 solution

--- src/app/assignment6/app.component.ts
+++ src/app/assignment6/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // ViewChild is used here for getting the onInspection() state only !
+  // in onSubmit() the local form reference of the template is used
+  @ViewChild('f') formData: NgForm
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  subscriptionDefault = this.subscriptions[0];
+
+  onSubmit(f: NgForm) {
+    console.log(f);
+    console.log(f.value.email);
+    console.log(f.value.password);
+    console.log(f.value.subscription);
+  }
+
+  onInspect() {
+    console.log("email valid: ", this.formData.controls.email.valid);
+    console.log("password valid:", this.formData.controls.password.valid);
+    //console.log(this.formData);
+  }
 
 }
